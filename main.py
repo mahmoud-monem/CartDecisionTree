fdb6558c51ee8bc55afbb2b5203be1d0312f6ef9
Mahmoud Monem <hooodamonem111@gmail.com>
2020-12-22 18:52:30 +0200

    prediction done

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 
 def read_data(files: list) -> pd.DataFrame:
     try:
-        data_frames = [pd.read_csv(f) for f in files]
+        data_frames = [pd.read_csv(f, sep=";") for f in files]
     except FileExistsError as e:
         print(e)
     data = pd.concat(data_frames, ignore_index=True)
@@ -28,10 +28,6 @@ def main():
     print("Reading data.......")
     data = read_data(["cardio_train.csv"])
     print("Done!")
-    print("Cleaning data......")
-    data = clean_data(data)
-    print("Done!")
-    data.to_excel("cardio_train.csv", index=False)
     model = CartDecisionTree(data, "cardio")
     print("Training model.....")
     # model.fit()
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+
 class Node:
     def __init__(self, label=str()):
         self.label = label
@@ -20,13 +21,15 @@ class CartDecisionTree:
         self.root = Node()
         self.classes = set()
         for idx in range(len(self.data)):
-            self.classes.add(self.data[idx][targetVariable])
+            self.classes.add(self.data[targetVariable][idx])
 
     def fit(self):
         indices = np.arange(0, len(self.data))
 
         unUsedAttributes = list()
         for attribute in self.data:
+            if attribute == self.targetVariable or attribute == "id":
+                continue
             unUsedAttributes.append(attribute)
 
         self.build(self.root, indices, unUsedAttributes)
@@ -34,9 +37,11 @@ class CartDecisionTree:
     def build(self, node: Node, indices: np.ndarray, unUsedAttributes: list):
         nodeLabel, attributeValues = self.calculateGiniIndex(indices, unUsedAttributes)
         node.setLabel(nodeLabel)
+        # print(nodeLabel)
 
         for value in attributeValues:
             flag = False
+            # print(attributeValues[value][1], attributeValues[value][0], attributeValues[value]["total"])
             for classLabel in self.classes:
                 if attributeValues[value][classLabel] == attributeValues[value]["total"]:
                     node.addEdge(value, classLabel)
@@ -48,6 +53,14 @@ class CartDecisionTree:
 
             node.addEdge(value)
             newUnUsed = list()
+            newIndices = np.array([])
+            idx2 = 0
+            for idx in indices:
+                if idx2 < len(attributeValues[value]["indices"]):
+                    if idx == attributeValues[value]["indices"][idx2]:
+                        idx2 += 1
+                        continue
+                    newIndices = np.append(newIndices, [idx])
             for attribute in unUsedAttributes:
                 if attribute == nodeLabel:
                     continue
@@ -62,16 +75,18 @@ class CartDecisionTree:
         for attribute in unUsedAttributes:
             attributeValues = dict()
             for idx in indices:
-                val = self.data[idx][attribute]
+                val = self.data[attribute][idx]
                 if attributeValues.get(val):
                     attributeValues[val]["total"] += 1
-                    attributeValues[val][self.data[idx][self.targetVariable]] += 1
-                    np.append(attributeValues[val]["indices"], idx)
+                    attributeValues[val][self.data[self.targetVariable][idx]] += 1
+                    attributeValues[val]["indices"] = np.append(attributeValues[val]["indices"], idx)
                 else:
-                    attributeValues[val] = {"total": 0}
-                    attributeValues[val] = {"indices": np.array()}
+                    attributeValues[val] = dict()
+                    attributeValues[val]["total"] = 1
+                    attributeValues[val]["indices"] = np.array([idx])
                     for classLabel in self.classes:
                         attributeValues[val][classLabel] = 0
+                    attributeValues[val][self.data[self.targetVariable][idx]] = 1
 
             giniIndex = 0
             for value in attributeValues:
@@ -87,3 +102,16 @@ class CartDecisionTree:
                 minIndexAttributeValues = attributeValues
 
         return minIndexLabel, minIndexAttributeValues
+
+    def predict(self, testData: pd.DataFrame) -> np.ndarray:
+        predicted = np.array([])
+        for idx in range(len(testData)):
+            self._predict(self.root, testData.loc[idx])
+        return predicted
+
+    def _predict(self, node: Node, testData):
+        if not node.edges:
+            return node.label
+        else:
+            for edge in node.edges:
+                return self._predict(edge, testData)
